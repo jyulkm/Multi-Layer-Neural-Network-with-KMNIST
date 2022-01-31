c21e215edf978b4b4f44dbe7a581dc7fc549d91d
Tae Kun Kim <taekunkim214@gmail.com>
2022-01-30 20:38:01 -0800

    implements main file

--- main.py
+++ main.py
@@ -3,13 +3,9 @@
 # Code snippet by Eric Yang Yu, Ajit Kumar, Savyasachi
 # Winter 2022
 ################################################################################
-# To install PyYaml, refer to the instructions for your system:
-# https://pyyaml.org/wiki/PyYAMLDocumentation
-################################################################################
-# If you don't have NumPy installed, please use the instructions here:
-# https://scipy.org/install.html
-################################################################################
+
 import argparse
+import numpy as np
 
 from data import load_data, load_config, generate_k_fold_set, z_score_normalize, one_hot_encoding
 from train import *
@@ -39,21 +35,35 @@ if __name__ == "__main__":
     config = load_config("./config.yaml")
 
     # Load the data
-    train_data, (x_test, y_test) = load_data(), load_data(train=False)
+    train_data, (x_test, t_test) = load_data(), load_data(train=False)
 
     # Create validation set out of training data.
-    x_train, y_train, x_val, y_val = None, None, None, None
+    x_train, t_train = train_data
+
+    x_train = np.random.shuffle(x_train)
+    t_train = np.random.shuffle(t_train)
+
+    N = x_train.shape[0]
+    a = int(np.round(N*0.8))
+    x_train, t_train, x_val, t_val = x_train[:
+                                             a], t_train[:a], x_train[a:], t_train[a:]
 
     # Any pre-processing on the datasets goes here.
+    x_train, _ = z_score_normalize(x_train)
+    x_val, _ = z_score_normalize(x_val)
+    x_test, _ = z_score_normalize(x_test)
 
     # Run the writeup experiments here
     if args.train_mlp:
-        train_mlp(x_train, y_train, x_val, y_val, x_test, y_test, config)
+        train_mlp(x_train, t_train, x_val, t_val, x_test, t_test, config)
     if args.check_gradients:
-        check_gradients(x_train, y_train, config)
+        check_gradients(x_train, t_train, config)
     if args.regularization:
-        regularization_experiment(x_train, y_train, x_val, y_val, x_test, y_test, config)
+        regularization_experiment(
+            x_train, t_train, x_val, t_val, x_test, t_test, config)
     if args.activation:
-        activation_experiment(x_train, y_train, x_val, y_val, x_test, y_test, config)
+        activation_experiment(x_train, t_train, x_val,
+                              t_val, x_test, t_test, config)
     if args.topology:
-        topology_experiment(x_train, y_train, x_val, y_val, x_test, y_test, config)
+        topology_experiment(x_train, t_train, x_val,
+                            t_val, x_test, t_test, config)
