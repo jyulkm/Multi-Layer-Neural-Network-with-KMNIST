corrects back propagation, softmax, and cross entropy, and partially changes the main script. Implements the train and test functions of train.py, partially.

main.py
@@ -13,7 +13,7 @@ from train import *
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--train_mlp', dest='train_mlp', action='store_true', default=False,
+    parser.add_argument('--train_mlp', dest='train_mlp', action='store_true', default=True,  # set to True for train_mlp
                         help='Train a single multi-layer perceptron using configs provided in config.yaml')
     parser.add_argument('--check_gradients', dest='check_gradients', action='store_true', default=False,
                         help='Check the network gradients computed by comparing the gradient computed using'
@@ -40,18 +40,20 @@ if __name__ == "__main__":
     # Create validation set out of training data.
     x_train, t_train = train_data
 
-    x_train = np.random.shuffle(x_train)
-    t_train = np.random.shuffle(t_train)
-
     N = x_train.shape[0]
+
+    shuffled_indices = np.random.permutation(N)
+    x_train = x_train[shuffled_indices]
+    t_train = t_train[shuffled_indices]
+
     a = int(np.round(N*0.8))
     x_train, t_train, x_val, t_val = x_train[:
                                              a], t_train[:a], x_train[a:], t_train[a:]
 
     # Any pre-processing on the datasets goes here.
-    x_train, _ = z_score_normalize(x_train)
-    x_val, _ = z_score_normalize(x_val)
-    x_test, _ = z_score_normalize(x_test)
+    x_train, (mean, sd) = z_score_normalize(x_train)
+    x_val, _ = z_score_normalize(x_val, mean, sd)
+    x_test, _ = z_score_normalize(x_test, mean, sd)
 
     t_train = one_hot_encoding(t_train)
     t_val = one_hot_encoding(t_val)

neuralnet.py
@@ -4,8 +4,10 @@
 # Winter 2022
 ################################################################################
 
+from __future__ import print_function
 import numpy as np
 import math
+
 import data
 
 
@@ -58,7 +60,7 @@ class Activation:
 
     def backward(self, delta):
         """
-        Takes in the weighted sum delta. 
+        Takes in the weighted sum delta.
         Compute the backward pass.
         """
         if self.activation_type == "sigmoid":
@@ -165,12 +167,17 @@ class Layer:
         Return self.d_x
         """
         self.d_w = self.x.T @ delta
-
         self.d_x = delta @ self.w.T
-
-        self.d_b = np.sum(delta)
+        self.d_b = np.sum(delta, axis=0)
 
         return self.d_x
+
+    def update_weights(self, learning_rate, momentum=None):
+        if momentum != None:
+            pass
+
+        self.w += learning_rate * self.d_w
+        self.b += learning_rate * self.d_b
 
 
 class NeuralNetwork:
@@ -192,7 +199,7 @@ class NeuralNetwork:
         self.y = None  # Save the output vector of model in this
         self.targets = None  # Save the targets in forward in this variable
         self.loss = None  # cross entropy performed on
-        self.loss_argmax = None
+        self.delta_k = None  # output layer delta
 
         self.config = config
         self.learning_rate = self.config['learning_rate']
@@ -217,7 +224,6 @@ class NeuralNetwork:
         """
 
         self.x = x
-        self.targets = targets
 
         z = self.x
         a = None
@@ -230,47 +236,40 @@ class NeuralNetwork:
 
         self.y = self.softmax(a)
 
-        if not isinstance(self.targets, type(None)):
-            # implement using the cross entropy function below
-            self.loss = self.targets - self.y
+        if not isinstance(targets, type(None)):
+            self.targets = targets
+            self.loss = self.cross_entropy(self.y, self.targets)
+            self.delta_k = self.targets - \
+                data.one_hot_encoding(np.argmax(self.y, axis=1))
 
-        return self.y, self.loss
+        self.y_labels = np.argmax(z, axis=1)
+
+        return self.y_labels, self.loss
 
     def backward(self):
         """
         Implement backpropagation here.
         Call backward methods of individual layer's.
         """
-        i = len(self.layers) - 1
-        delta_k = self.loss
+        # update deltas:
+        delta = self.delta_k
+        for layer in self.layers[::-1]:
+            delta = layer.backward(delta)
 
-        self.backward_recur(i, delta_k)
-
-    def backward_recur(self, i, delta_k):
-        # layer backward
-        self.layers[i].backward(delta_k)
-        self.layers[i].w -= self.config['learning_rate'] * self.layers[i].d_w
-        self.layers[i].b -= self.config['learning_rate'] * self.layers[i].d_b
-
-        # activation backward
-        if i > 0:
-            weighted_sum_delta = self.layers[i].d_x
-            delta_j = self.layers[i-1].backward(weighted_sum_delta)
-
-            self.backward_recur(i=i-2, delta_k=delta_j)
+        # update weights
+        for layer in self.layers[::-2]:
+            layer.update_weights(self.learning_rate)
 
     def softmax(self, a):
         """
         Implement the softmax function here.
         Remember to take care of the overflow condition.
         """
-        return np.exp(a) / np.sum(np.exp(a))
+        def subtract_max(a): return a - max(a)
+        return np.exp(a) / np.sum(np.exp(np.apply_along_axis(subtract_max, 1, a)))
 
     def cross_entropy(self, logits, targets):
         """
         Compute the categorical cross-entropy loss and return it.
         """
-        y_categories = np.max(logits, axis=1)
-        targets = data.onehot_decode(targets)
-
-        return -1 * np.dot(targets, np.log(y_categories))
+        return -np.sum(targets * np.log(logits))

train.py
@@ -31,9 +31,25 @@ def train(x_train, t_train, x_val, t_val, config, experiment=None):
     val_loss = []
     best_model = None
 
-    x_train = z_score_normalize(x_train)
+    N = len(x_train)  # number of examples
 
     model = NeuralNetwork(config=config)
+    for epoch in config["epochs"]:
+
+        shuffled_indices = np.random.permutation(N)
+        x_train = x_train[shuffled_indices]
+        t_train = t_train[shuffled_indices]
+
+        minibatch_loss = []  # saves the loss over all minibatches
+        minibatch_acc = []  # saves the accuracy over all minibatches
+        for minibatch in data.generate_minibatches(x_train, t_train, batch_size=config["batch_size"]):
+            x, t = minibatch
+            loss, accuracy = test(model, x, t)  # performs forward propagation
+
+            minibatch_loss.append(loss)
+            minibatch_acc.append(accuracy)
+
+            model.backward()
 
     # return train_acc, val_acc, train_loss, val_loss, best_model
     raise NotImplementedError('Train not implemented')
@@ -52,7 +68,10 @@ def test(model, x_test, t_test):
         Loss, Test accuracy
     """
     # return loss, accuracy
-    raise NotImplementedError('Test not implemented')
+    y_labels, loss = model(x_test, t_test)
+    accuracy = np.mean(y_labels == np.argmax(t_test, axis=1))
+
+    return loss, accuracy
 
 
 def train_mlp(x_train, t_train, x_val, t_val, x_test, t_test, config):
@@ -130,8 +149,8 @@ def get_approx(e, model_B, x_train, t_train, backprop_vals):
                         [layer, kind[0], str(i), str(0)])]
                     approx_val = (E1 - E0) / (2*e)
 
-                    print("backprop val: " + str(backprop_val))
-                    print("approx val: " + str(approx_val))
+                    # print("backprop val: " + str(backprop_val))
+                    # print("approx val: " + str(approx_val))
 
                     diff = np.abs(backprop_val - approx_val)
 
@@ -149,13 +168,14 @@ def get_approx(e, model_B, x_train, t_train, backprop_vals):
                 backprop_val = backprop_vals["_".join([layer, kind[0]])]
                 approx_val = (E1 - E0) / (2*e)
 
-                print(backprop_val > 0)
-                print("approx val: " + str(approx_val*1000000))
+                # print("backprop val: " + str(backprop_val))
+                # print("approx val: " + str(approx_val*1000000))
 
                 diff = np.abs(backprop_val - approx_val)
 
                 diffs.append(diff)
 
+    print(diffs)
     return np.all(np.array(diffs) <= e**2)
 
 
@@ -186,12 +206,12 @@ def initialize_weight(model_B, layer, kind, e=0, i=None, j=None):
         if layer == "input_hidden":
             input_hidden_A.w[i][j] += e
         if layer == "hidden_output":
-            hidden_output_B.w[i][j] += e
+            hidden_output_A.w[i][j] += e
     if kind == "bias":
         if layer == "input_hidden":
             input_hidden_A.b[i] += e
         if layer == "hidden_output":
-            hidden_output_B.b[i] += e
+            hidden_output_A.b[i] += e
 
     return model_A
 
@@ -208,7 +228,6 @@ def check_gradients(x_train, t_train, config):
 
     # initializes the model
     model = NeuralNetwork(config=config)
-    y = model(x_train, t_train)
 
     # saves the layers of the model
     # Selecting the input to hidden layer object
@@ -217,23 +236,24 @@ def check_gradients(x_train, t_train, config):
     hidden_output = model.layers[2]
 
     # saves the initial bias terms of each layer
-    input_hidden_b0 = input_hidden.b
-    hidden_output_b0 = hidden_output.b
+    input_hidden_b0 = input_hidden.b.copy()
+    hidden_output_b0 = hidden_output.b.copy()
 
     # saves the initial weight matrices of each layer
-    input_hidden_w0 = input_hidden.w
-    hidden_output_w0 = hidden_output.w
+    input_hidden_w0 = input_hidden.w.copy()
+    hidden_output_w0 = hidden_output.w.copy()
 
-    # performs backward propagation
+    # performs forward and backward propagations
+    model(x_train, t_train)
     model.backward()
 
     # saves the initial bias terms of each layer
-    input_hidden_b1 = input_hidden.b
-    hidden_output_b1 = hidden_output.b
+    input_hidden_b1 = input_hidden.b.copy()
+    hidden_output_b1 = hidden_output.b.copy()
 
     # saves the initial weight matrices of each layer
-    input_hidden_w1 = input_hidden.w
-    hidden_output_w1 = hidden_output.w
+    input_hidden_w1 = input_hidden.w.copy()
+    hidden_output_w1 = hidden_output.w.copy()
 
     # calculates the gradients of the weight matrices
     input_hidden_wg = (input_hidden_w1 - input_hidden_w0) / model.learning_rate
@@ -244,8 +264,8 @@ def check_gradients(x_train, t_train, config):
     input_hidden_bg = (input_hidden_b1[0] -
                        input_hidden_b0[0]) / model.learning_rate
 
-    hidden_output_bg = (hidden_output_b1 -
-                        hidden_output_b0) / model.learning_rate
+    hidden_output_bg = (hidden_output_b1[0] -
+                        hidden_output_b0[0]) / model.learning_rate
 
     backprop_vals = {
         "input_hidden_w_0_0": input_hidden_wg[0][0],
@@ -263,7 +283,16 @@ def check_gradients(x_train, t_train, config):
 
 
 config = load_config("./config.yaml")
+
+
 x_train = load_data()[0]
 t_train = load_data()[1]
+
+N = len(x_train)
+
+shuffled_indices = np.random.permutation(N)
+x_train = x_train[shuffled_indices]
+t_train = t_train[shuffled_indices]
+
 
 check_gradients(x_train, t_train, config)